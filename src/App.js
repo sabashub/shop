1bfd469a724bf8ad4a74625128577db7c509e45d
sabashub <shubitidzesaba09@gmail.com>
2023-03-23 20:23:06 +0400

    initialize categories container

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import "./categories.styles.scss"
 function App() {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 1,
+      title: "Jackets",
+    },
+    {
+      id: 1,
+      title: "Sneakers",
+    },
+    {
+      id: 1,
+      title: "Womens",
+    },
+    {
+      id: 1,
+      title: "Mens",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      {categories.map(({ title }) => (
+        <div className="category-container">
+          <img />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>shop now</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
