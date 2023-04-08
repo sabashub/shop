ccbad17bfd998816e7b4637563e6668259842f99
sabashub <shubitidzesaba09@gmail.com>
2023-04-08 15:30:29 +0400

    fix bug

--- src/components/Directory-Item/DirectoryItem.jsx
+++ src/components/Directory-Item/DirectoryItem.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import {
   DirectoryItemContainer,
   BackgroundImage,
-  Bodym,
   Body,
 } from "./DirectoryItem.styles.jsx";
 const DirectoryItem = ({ category }) => {
